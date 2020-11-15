244dce64bf1ef871998a09d3eadb3b75ea2a64ad
seungjun-dev <seungjun91@naver.com>
2020-11-15 21:47:40 +0900

    parse league info

--- js/riot-api.js
+++ js/riot-api.js
@@ -1,32 +1,31 @@
 const API_KEY = "RGAPI-68f5f184-ac86-42c5-be19-a66ea8075840";
 
-
-function getSummerInfo() {
+function getSummonerInfo() {
     var name = document.getElementById("userName").value;
-    
+
     //console.log(name);
-    
+
     fetch(
         //`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%EC%9F%A4%20%EC%9E%84%EC%8A%A4?api_key=RGAPI-68f5f184-ac86-42c5-be19-a66ea8075840`
         `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${API_KEY}`
     )
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(json){
-        /*
-        SUMMONER-V4 
-        json.id:
-        json.accountId:
-        json.puuid:
-        json.name:
-        json.profileIconId:
-        json.revisionDate:
-        json.summonerLevel:
-        */
-        console.log(json.id);
-        getLeagueInfo(json.id);
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            /*
+            SUMMONER-V4 
+            json.id:
+            json.accountId:
+            json.puuid:
+            json.name:
+            json.profileIconId:
+            json.revisionDate:
+            json.summonerLevel:
+            */
+            //console.log(json.id);
+            getLeagueInfo(json.id);
+        });
 }
 
 function getLeagueInfo(id) {
@@ -36,16 +35,30 @@ function getLeagueInfo(id) {
         //`https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/9lZSMawwYs8WUyjQUFLlVGWLLrMu598L_RRdniS3tsOXFQ?api_key=RGAPI-68f5f184-ac86-42c5-be19-a66ea8075840`
         `https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${encryptedId}?api_key=${API_KEY}`
     )
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(json){
-        console.log(json);
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            console.log(json[0].summonerName);
+            /*
+            LEAGUE-V4
+            json[0] -> 자유랭크
+            json[1] -> 솔로랭크
+            json[i].tier;
+            json[i].rank;
+            json[i].leaguePoints;
+            json[i].wins;
+            json[i].losses;
+            json[i].veteran;
+            json[i].inactive;
+            json[i].freshBlood;
+            json[i].hotStreak;
+            */
+        });
 }
 
 function init() {
-    console.log("riot api start");    
+    console.log("riot api start");
 }
 
 init();
